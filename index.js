93244383d94d1e6616f64aff67ec4d08b284e755
mohammed sarfaraz <mdsarfaraz2928@gmail.com>
2024-03-03 13:39:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var imgSource1 = "images/dice" + randomNumber1 + ".png";
+document.querySelectorAll("img")[0].setAttribute("src" , imgSource1);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var imgSource2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src" , imgSource2);
+
+if(randomNumber1 == randomNumber2){
+    document.querySelector("h1").textContent = "its a tie";
+}
+else if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").textContent = "player 1 win";
+
+}
+else
+{
+    document.querySelector("h1").textContent = "player 2 win";
+
+}
